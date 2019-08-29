8608db568dd20920e36945e3f3b5d1c5a1e03ead
vladislav <gradiusalisik@gmail.com>
2019-08-30 00:43:28 +0300

    добавил дефолтное значение

--- src/components/CardServicesInside/CardServicesInside.js
+++ src/components/CardServicesInside/CardServicesInside.js
@@ -4,7 +4,8 @@ import Price from "../Price/Price";
 import {
   CardServicesInsideStyled,
   Title,
-  Description
+  Description,
+  Text
 } from "./CardServicesInside.styled";
 
 const CardServicesInside = ({ title, description, price, className }) => (
@@ -12,6 +13,7 @@ const CardServicesInside = ({ title, description, price, className }) => (
     <Title>{title}</Title>
     <Description>{description}</Description>
 
+    {price ? <Price price={price} /> : <Text>Бесплатно</Text>}
     <Price price={price} />
   </CardServicesInsideStyled>
 );
